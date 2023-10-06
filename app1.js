8b97fd9354263635693013099c6a943d9580360d
Aditya Tripathi <adityatripathi3906@gmail.com>
2023-10-06 20:24:48 +0530

    template literals

--- app1.js
+++ app1.js
@@ -2,5 +2,6 @@ console.log("Hello World");
 console.log("CG was Here");
 let a = 10;
 let b = 20;
-let c = a+b;
+//template literals
+let c = console.log(`the sum of a and b is ${a+b} rupees.`);
 console.log(c);
