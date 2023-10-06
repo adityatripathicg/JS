fc5d326f09a19f29a6ba8d4cac112d4ec4f302e1
Aditya Tripathi <adityatripathi3906@gmail.com>
2023-10-06 22:25:26 +0530

    if

--- app1.js
+++ app1.js
@@ -5,3 +5,8 @@ let b = 20;
 //template literals
 let c = console.log(`the sum of a and b is ${a+b} rupees.`);
 console.log(c);
+//conditional statements
+let name = "CG";
+if(name == "CG"){
+    console.log("Welcome CG");
+}
