49b57728edecf44a1def1c66d623408b8e3c8211
Aditya Tripathi <adityatripathi3906@gmail.com>
2023-10-07 21:54:11 +0530

    string methods

---
+++ stringmethods.js
@@ -0,0 +1,5 @@
+let msg = "help!";
+console.log("the trimmed and uppercased msg is ", msg.trim().toUpperCase());
+//college to cottege
+let str = "apnacollege";
+console.log(str.slice(4).replace("l","t").replace("l","t"));
