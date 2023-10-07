1c2fdf8d9fdd96bea9a9d571e135691316e35d3d
Aditya Tripathi <adityatripathi3906@gmail.com>
2023-10-07 20:38:40 +0530

    string trim

---
+++ stringtrim.js
@@ -0,0 +1,6 @@
+let msg = "     hello     ";
+console.log("the original str is ", msg);
+console.log("the trimmed str is ", msg.trim());
+console.log("please set a password");
+let pass = prompt("Enter your password");
+console.log("the password is ", pass.trim());
