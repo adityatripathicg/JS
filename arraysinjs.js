398a6a2f37ce158d68d99b2354a48921dbe8eb68
Aditya Tripathi <adityatripathi3906@gmail.com>
2023-10-08 00:22:20 +0530

    splice and index of and reverse

--- arraysinjs.js
+++ arraysinjs.js
@@ -6,3 +6,11 @@ let month = ["january","july","march","august"];
 month[0] = month[1];
 month[1] = "june";
 console.log(month);
+//ass q2
+let start = ["january","july","march","august"];
+start.splice(0,1);
+start.splice(1,0,"june");
+console.log(start);
+//index of array
+let lang = ["c","c++","html","javascript","python","java","c#","sql"];
+console.log(lang.reverse().indexOf("javascript")); 
