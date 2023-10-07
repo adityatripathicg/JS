68cbc89ea83c736e1ef5d35fe3344141c280da99
Aditya Tripathi <adityatripathi3906@gmail.com>
2023-10-07 23:44:42 +0530

    ass qs

---
+++ arraysinjs.js
@@ -0,0 +1,8 @@
+let arr = ["AdityaxCG",20,5.10];
+console.log(arr[0][7]);
+console.log(arr);
+//array q
+let month = ["january","july","march","august"];
+month[0] = month[1];
+month[1] = "june";
+console.log(month);
