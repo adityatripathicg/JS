0d204df6d1ddd692abaadc88694276d3efab3b7b
Aditya Tripathi <adityatripathi3906@gmail.com>
2023-10-08 11:01:01 +0530

    up

--- 15qs.js
+++ 15qs.js
@@ -7,9 +7,9 @@ else{
     console.log("bad");
 }
 //2
-// let name = prompt("ENTER NAME");
-// let age = prompt("ENTER AGE");
-// console.log(`${name} is ${age} years old.`);
+let name = prompt("ENTER NAME");
+let age = prompt("ENTER AGE");
+console.log(`${name} is ${age} years old.`);
 //3
 let quarter = 1;//prompt("ENTER QUARTER NUMBER");
 switch(quarter){
@@ -46,4 +46,14 @@ else if(b>a && b>c){
 }
 else{
     console.log("the largest num is ", c);
-}   
+}
+
+//6
+let num1 = 221;
+let num2 = 222221;
+if((num1%10)==(num2%10)){
+    console.log("both have same last digit");
+}
+else{
+    console.log("both do not have same last digit");
+}
