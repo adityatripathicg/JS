daaf25a24583c8aea40e0cd7acfddffe59e466b8
Aditya Tripathi <adityatripathi3906@gmail.com>
2023-10-08 09:22:32 +0530

    switch

--- 15qs.js
+++ 15qs.js
@@ -1,8 +1,28 @@
 //1
-let num = 100;
+let num = 1011;
 if(num%10===0){
     console.log("good");
 }
 else{
     console.log("bad");
+}
+//2
+let name = prompt("ENTER NAME");
+let age = prompt("ENTER AGE");
+console.log(`${name} is ${age} years old.`)
+//3
+let quarter = 1;//prompt("ENTER QUARTER NUMBER");
+switch(quarter){
+    case 1 :
+        console.log("January","February","March");
+        break;
+    case 2 :
+        console.log("April","May","June"); 
+        break;  
+    case 3 :
+        console.log("July","August","September");
+        break;
+    case 4 :
+        console.log("October","November","December");        
+        break; 
 }
