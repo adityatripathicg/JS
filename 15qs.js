3fe692ae34e3480a1eb71ff81c08c1a6ed66bdab
Aditya Tripathi <adityatripathi3906@gmail.com>
2023-10-08 09:46:55 +0530

    golden string and largest of 3

--- 15qs.js
+++ 15qs.js
@@ -7,9 +7,9 @@ else{
     console.log("bad");
 }
 //2
-let name = prompt("ENTER NAME");
-let age = prompt("ENTER AGE");
-console.log(`${name} is ${age} years old.`)
+// let name = prompt("ENTER NAME");
+// let age = prompt("ENTER AGE");
+// console.log(`${name} is ${age} years old.`);
 //3
 let quarter = 1;//prompt("ENTER QUARTER NUMBER");
 switch(quarter){
@@ -25,4 +25,25 @@ switch(quarter){
     case 4 :
         console.log("October","November","December");        
         break; 
+}
+//4
+let str = "A big fat hen.";
+if(str[0] =='a' || str[0]=='A' && str.length>5){
+    console.log("Golden String");
+}
+else{
+    console.log("Not a Golden String");
+}
+//5
+let a = 10;
+let b = 20;
+let c = 30;
+if(a>b && a>c){
+    console.log("the largest num is ", a);
+}
+else if(b>a && b>c){
+    console.log("the largest num is ", b);
+}
+else{
+    console.log("the largest num is ", c);
 }
