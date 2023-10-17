a897bb4c84be156c130ae8cda770e432b76bf19a
Aditya Tripathi <adityatripathi3906@gmail.com>
2023-10-17 19:30:06 +0530

    keyboard events, form events, form data extraction

--- DOM23/domevents.js
+++ DOM23/domevents.js
@@ -1,6 +1,6 @@
 
 let btn2 = document.querySelector("button");
-btn2.addEventListener("click", sayhello);
+btn2.addEventListener("mouseout", sayhello);
 // btn2.onclick = sayhello;
 // btn2.onmouseenter = function () {
 //     console.log("mouse hover recorded!")
@@ -9,3 +9,32 @@ btn2.addEventListener("click", sayhello);
     console.log("button was clicked!")
     alert("Hello There!");
 };
+
+
+//keyboard events
+let inp = document.querySelector("input");
+inp.addEventListener("keydown", function (events) {
+    console.log(events.code);
+    console.log(events.key);    
+    console.log("Key was pressed!");
+});
+
+//form events
+let form = document.querySelector("form");
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+    console.log("Form was submitted!");
+    //extracting form data
+    // let user = document.querySelector("#user");
+    // let pass = document.querySelector("#pass");
+    let user = this.elements[0];
+    let pass = this.elements[1];
+    console.log(user.value);
+    console.log(pass.value);
+});
+
+
+
+
+
+
