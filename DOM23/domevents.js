d6a12cf7bd1d1f29d1236366160cf718337a3379
Aditya Tripathi <adityatripathi3906@gmail.com>
2023-10-17 11:53:29 +0530

    addEventListener

--- DOM23/domevents.js
+++ DOM23/domevents.js
@@ -1,15 +1,10 @@
-// let btn = document.createElement("button");
-// let body = document.querySelector("body");
-// btn.innerHTML = "Hello";
+
 let btn2 = document.querySelector("button");
-// body.appendChild(btn);
-btn2.onclick = sayhello;
-btn2.onmouseenter = function () {
-    console.log("mouse hover recorded!")
-}
-// function () {
-//     console.log("Hello!")
-// };
+btn2.addEventListener("click", sayhello);
+// btn2.onclick = sayhello;
+// btn2.onmouseenter = function () {
+//     console.log("mouse hover recorded!")
+// }
  function sayhello() {
     console.log("button was clicked!")
     alert("Hello There!");
