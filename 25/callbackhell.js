dccad781d68660c98f69f969520d3511ed48f38b
Aditya Tripathi <adityatripathi3906@gmail.com>
2023-10-19 13:05:32 +0530

    callback hell using promises

--- 25/callbackhell.js
+++ 25/callbackhell.js
@@ -1,15 +1,26 @@
 let h1 = document.querySelector("h1");
-function changecolor(color,delay,nextcolor){
-    setTimeout(()=>{
-        h1.style.color = color;
-        if(nextcolor) nextcolor();
-    },delay);
+function changecolor(color,delay){
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            h1.style.color = color;
+            resolve();
+        },delay);
+    });
 }
 
-    changecolor("red",1000,()=>{
-        changecolor("orange",1000,()=>{
-            changecolor("Green",1000,()=>{
-                changecolor("red",1000);
-            })
-        })
+    changecolor("red",1000)
+    .then(()=>{
+        console.log("Red done");
+        return changecolor("orange",1000);
     })
+    .then(()=>{
+        console.log("orange done")
+        return changecolor("blue",1000);
+    })
+    .then(()=>{
+        console.log("Blue done!");
+    })
+
+    
+    // changecolor("Green",1000);
+    // changecolor("red",1000);
