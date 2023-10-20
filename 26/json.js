2372619efe06a36497d48b0643473a69878b803d
Aditya Tripathi <adityatripathi3906@gmail.com>
2023-10-20 23:56:00 +0530

    Json in api's

---
+++ 26/json.js
@@ -0,0 +1,8 @@
+let jsonres = '{"fact":"Cats lap liquid from the underside of their tongue, not from the top.","length":69}'
+let parsed = JSON.parse(jsonres);
+let url = "https://catfact.ninja/fact";
+fetch(url)
+.then((res)=>{
+    console.log(res);
+    console.log(res.json());
+})
