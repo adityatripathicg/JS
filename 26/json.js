378279b7c8205ecace7750faa54e3caeda6a0850
Aditya Tripathi <adityatripathi3906@gmail.com>
2023-10-21 10:39:05 +0530

    btn click shows a new facts also used bootstrap

--- 26/json.js
+++ 26/json.js
@@ -1,30 +1,41 @@
-let jsonres = '{"fact":"Cats lap liquid from the underside of their tongue, not from the top.","length":69}'
-let parsed = JSON.parse(jsonres);
-let url = "https://catfact.ninja/fact";
-fetch(url)
-.then((res)=>{
-    console.log(res);
-    return res.json();
-})
-.then((data)=>{
-    console.log(data);
-})
-.catch((err)=>{
-    console.log(err);
-})
+// let jsonres = '{"fact":"Cats lap liquid from the underside of their tongue, not from the top.","length":69}'
+// let parsed = JSON.parse(jsonres);
+ let url = "https://catfact.ninja/fact";
+// fetch(url)
+// .then((res)=>{
+//     console.log(res);
+//     return res.json();
+// })
+// .then((data)=>{
+//     console.log(data);
+// })
+// .catch((err)=>{
+//     console.log(err);
+// })
 
 
-//fetch with async and await
-async function getfacts(){
-    let res = await fetch(url);
-    let data = await res.json();
-    console.log(data.fact);
+// //fetch with async and await
+// async function getfacts(){
+//     let res = await fetch(url);
+//     let data = await res.json();
+//     console.log(data.fact);
+// }
+// getfacts();
+
+// //axios library
+// async function gf(){
+//     let res = await axios.get(url);
+//     console.log(res.data.fact);
+// }
+//gf();
+async function facts(){
+    let facts = await axios.get(url);
+    return facts.data.fact;
 }
-getfacts();
-
-//axios library
-async function gf(){
-    let res = await axios.get(url);
-    console.log(res.data.fact);
-}
-gf();
+let btn = document.querySelector("button");
+let p = document.querySelector("#fact");
+btn.addEventListener("click", async()=>{
+    let fact = await facts();
+    console.log(fact);
+    p.innerText = fact;
+})
